14718b99f021d9907a26720b6d3508339e4c0d7c
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-05-30 15:31:29 +0530

    Create Flask_Basics.py

---
+++ AIML_Project_8/Flask_Basics.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask
+
+#Instantiate the flask app
+app = Flask(__name__)
+
+@app.route('/') # home directory
+def hello():
+    return 'I am learning AIML congrats to myself'
+
+# To run the app you must use the run() function();
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
